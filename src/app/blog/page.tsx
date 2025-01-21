61b032180bcab4e676026547125406cb2b965d32
menneck <zeeeee.peeeeetryyy@gmail.com>
2025-01-20 22:31:47 -0300

    feat(blog): Add my first article of blog and make them rendered in blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,30 @@
+import fs from "fs";
+import { marked } from "marked";
+import path from "path";
+
 export default function Blog() {
+  const postsDirectory = path.join(process.cwd(), "public/posts");
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const posts = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    return {
+      content: marked.parse(fileContents),
+      title: filename.replace(".md", ""),
+    };
+  });
+
+  console.log(posts);
+
   return (
-    <>
-      <h1>Blog Page</h1>
-    </>
+    <section className="flex flex-col justify-center items-center w-screen">
+      {posts.map((post, index) => (
+        <div key={index} className="post">
+          <h1 className="text-2xl font-bold">{post.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        </div>
+      ))}
+    </section>
   );
 }
